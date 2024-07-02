2443d3a763e55c5c63d39b8521ec2b3bee14c06c
Steven Reddy <steveng.reddy@gmail.com>
2024-07-02 17:14:47 +0900

    fixed: webpack config files

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,11 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-        static: './dist',
-        watchFiles: ['src/**/*.js', 'public/**/*'],
-    },
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+    watchFiles: ["src/**/*.js", "public/**/*"],
+  },
 });
